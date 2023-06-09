120c003cf389f824ab507e219135881b7cc112a8
Jonathan Wider <jonathan.wider@ufz.de>
2023-06-09 10:22:34 +0200

    add parameters to submit scripts

--- submit_script_1_dataset_creation.sh
+++ submit_script_1_dataset_creation.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
 
-#SBATCH --job-name=PD_data_creation
+#SBATCH --job-name=CreateData
 #SBATCH --time=0-03:45:00
-#SBATCH --mem-per-cpu=16G
+#SBATCH --mem-per-cpu=32G
 
 # output files
-#SBATCH -o pd_ds_creation.out
-#SBATCH -e pd_ds_creation.err
+#SBATCH -o /data/compoundx/WeatherDiff/job_logs/%x-%u-%j.out
+#SBATCH -e /data/compoundx/WeatherDiff/job_logs/%x-%u-%j.err
 
+# begin reading command line arguments
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -p Path"
+   echo -e "\t-p The path to a template configuration file to use in the dataset creation."
+   exit 1 # Exit script after printing help
+}
+
+while getopts "p:" opt
+do
+   echo "$opt"
+   case "$opt" in
+      p ) ConfigPath="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$ConfigPath" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+# stop reading command line arguments
 
 module load Anaconda3/2020.07
 source $EBROOTANACONDA3/etc/profile.d/conda.sh
 
 conda activate TORCH311
 
-srun python s1_write_dataset.py  -cf "/data/compoundx/WeatherDiff/config_file/template_geopotential_500.yml"
+srun python s1_write_dataset.py  -cf $ConfigPath
--- submit_script_3_eval.sh
+++ submit_script_3_eval.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
-#SBATCH --job-name=PD_eval
+#SBATCH --job-name=EvalCond
 #SBATCH --time=0-03:45:00
 #SBATCH -G nvidia-a100:1
 #SBATCH --mem-per-cpu=16G
 # output files
-#SBATCH -o c_eval.out
-#SBATCH -e c_eval.err
+#SBATCH -o /data/compoundx/WeatherDiff/job_logs/%x-%u-%j.out
+#SBATCH -e /data/compoundx/WeatherDiff/job_logs/%x-%u-%j.err
 
+# begin reading command line arguments
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -d DatasetID -m ModelID"
+   echo -e "\t-d The ID of the dataset the model was trained on."
+   echo -e "\t-m The ID of the model the predictions were created with."
+   exit 1 # Exit script after printing help
+}
+
+while getopts "d:m:" opt
+do
+   echo "$opt"
+   case "$opt" in
+      d ) DatasetID="$OPTARG" ;;
+      m ) ModelID="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$DatasetID" ] || [ -z "$ModelID" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+# stop reading command line arguments
 
 module load Anaconda3/2020.07
 source $EBROOTANACONDA3/etc/profile.d/conda.sh
 
 conda activate TORCH311
 
-srun python s5_write_predictions_CPD.py -did 278771 -mid 011A3B
+srun python s5_write_predictions_CPD.py -did $DatasetID -mid $ModelID
