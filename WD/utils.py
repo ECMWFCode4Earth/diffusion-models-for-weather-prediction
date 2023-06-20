d5d0fc02606e90af24c69d750f679c759e5c4b56
anand <mohit.anand@ufz.de>
2023-06-20 15:42:16 +0200

    added lat based weight

--- WD/utils.py
+++ WD/utils.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import xarray as xr
 import numpy as np
 import uuid
+import h5py
+
+from torch import tensor
 
 
 def check_devices():
@@ -104,3 +107,69 @@ def n_condition_channels(config):
         else n_level
     )
     return n_level
+
+
+def norm_area(deg=5.625):
+    with h5py.File("/data/compoundx/WeatherBench/gridarea.h5", "r") as f:
+        return f[f"norm_gridarea_{int(360/deg)}x{int(180/deg)}"][:]
+
+
+class AreaWeightedMSELoss:
+    def __init__(self, spatial_res):
+        deg = float(spatial_res[:-3])
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.weight = torch.tensor(norm_area(deg=deg), device=device)
+
+    def loss_fn(self, input: tensor, target: tensor):
+        return torch.sum(self.weight * (input - target) ** 2)
+
+
+def comp_area_lat_lon(lat: np.array, lon: np.array) -> np.array:
+    """Calculate the area between an array of lat lons
+
+    Args:
+        lat (np.array): Array of latitudes
+        lon (np.array): Array of longitudes
+
+    Returns:
+        np.array: Area
+    """
+
+    radius = 6.37122e6  # in meters
+
+    lat = np.squeeze(lat)
+    lon = np.squeeze(lon)
+    nlat = len(lat)
+    nlon = len(lon)
+
+    # LATITUDE
+    lat_edge = np.zeros((nlat + 1))
+    lat_edge[0] = max(-90, lat[0] - 0.5 * (lat[1] - lat[0]))
+    lat_edge[1:nlat] = 0.5 * (lat[: nlat - 1] + lat[1:nlat])
+    lat_edge[nlat] = min(
+        90, lat[nlat - 1] - 0.5 * (lat[nlat - 2] - lat[nlat - 1])
+    )
+    dlat = np.diff(lat_edge)
+
+    # LONGITUDE
+    lon_edge = np.zeros((nlon + 1))
+    lon_edge[0] = lon[0] - 0.5 * (lon[1] - lon[0])
+    lon_edge[1:nlon] = 0.5 * (lon[: nlon - 1] + lon[1:nlon])
+    lon_edge[nlon] = lon[nlon - 1] - 0.5 * (lon[nlon - 2] - lon[nlon - 1])
+    dlon = np.diff(lon_edge)
+
+    dlon_2d, dlat_2d = np.meshgrid(
+        dlon, dlat
+    )  # create mesh with cell size in deg
+    lon_2d, lat_2d = np.meshgrid(lon, lat)
+
+    dy = radius * (dlat_2d * (np.pi / 180.0))
+    dx = radius * np.multiply(
+        dlon_2d * (np.pi / 180.0), np.cos(lat_2d * (np.pi / 180.0))
+    )
+
+    area = np.multiply(dx, dy)
+    if np.sum(area) < 0:
+        area = -1 * area
+
+    return area
