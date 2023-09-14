17c7c12f1041a8d8770fc836797162d871466734
anand <mohit.anand@ufz.de>
2023-09-14 18:03:00 +0200

    added ensemble metrics

--- WD/utils.py
+++ WD/utils.py
@@ -137,56 +137,6 @@ class AreaWeightedMSELoss(WeightedMSELoss):
         super().__init__(weights=comp_area_weights_simple(lat, lon))
     
 
-def comp_area_lat_lon(lat: np.array, lon: np.array) -> np.array:
-    """Calculate the area between an array of lat lons
-
-    Args:
-        lat (np.array): Array of latitudes
-        lon (np.array): Array of longitudes
-
-    Returns:
-        np.array: Area
-    """
-
-    radius = 6.37122e6  # in meters
-
-    lat = np.squeeze(lat)
-    lon = np.squeeze(lon)
-    nlat = len(lat)
-    nlon = len(lon)
-
-    # LATITUDE
-    lat_edge = np.zeros((nlat + 1))
-    lat_edge[0] = max(-90, lat[0] - 0.5 * (lat[1] - lat[0]))
-    lat_edge[1:nlat] = 0.5 * (lat[: nlat - 1] + lat[1:nlat])
-    lat_edge[nlat] = min(
-        90, lat[nlat - 1] - 0.5 * (lat[nlat - 2] - lat[nlat - 1])
-    )
-    dlat = np.diff(lat_edge)
-
-    # LONGITUDE
-    lon_edge = np.zeros((nlon + 1))
-    lon_edge[0] = lon[0] - 0.5 * (lon[1] - lon[0])
-    lon_edge[1:nlon] = 0.5 * (lon[: nlon - 1] + lon[1:nlon])
-    lon_edge[nlon] = lon[nlon - 1] - 0.5 * (lon[nlon - 2] - lon[nlon - 1])
-    dlon = np.diff(lon_edge)
-
-    dlon_2d, dlat_2d = np.meshgrid(
-        dlon, dlat
-    )  # create mesh with cell size in deg
-    lon_2d, lat_2d = np.meshgrid(lon, lat)
-
-    dy = radius * (dlat_2d * (np.pi / 180.0))
-    dx = radius * np.multiply(
-        dlon_2d * (np.pi / 180.0), np.cos(lat_2d * (np.pi / 180.0))
-    )
-
-    area = np.multiply(dx, dy)
-    if np.sum(area) < 0:
-        area = -1 * area
-
-    return area
-
 
 def comp_area_weights_simple(lat: np.ndarray, lon: np.ndarray) -> np.ndarray:
     """An easier way to calculate the (already normalized) area weights.
